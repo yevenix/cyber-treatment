f0e4dddf79fea79bdacbc8ccfff43678ee859365
yevenix <y6y77@qq.com>
2026-04-02 20:35:56 +0800

    项目的初始配置

---
+++ cyber-treatment/src/main/java/com/yevenix/cybertreatment/CyberTreatmentApplication.java
@@ -0,0 +1,13 @@
+package com.yevenix.cybertreatment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberTreatmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberTreatmentApplication.class, args);
+	}
+
+}
---
+++ cyber-treatment/src/test/java/com/yevenix/cybertreatment/CyberTreatmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yevenix.cybertreatment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberTreatmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
