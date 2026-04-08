d7623f92ee1ddc5642e91868740bf856067cdcc3
yevenix <y6y77@qq.com>
2026-04-08 14:10:50 +0800

    用户端-注册与登录-密码登录、手机号注册

--- cyber-treatment/src/main/java/com/yevenix/cybertreatment/mapper/UserMapper.java
+++ cyber-treatment/src/main/java/com/yevenix/cybertreatment/mapper/UserMapper.java
@@ -8,4 +8,5 @@ import org.apache.ibatis.annotations.Mapper;
  * 用户Mapper
  */
 @Mapper
-public interface UserMapper extends BaseMapper<User> {}
+public interface UserMapper extends BaseMapper<User> {
+}
